cdb03e102a32dfd60f4aadb4e28ecaa81b5ae5b8
cormacguerin <cormacguerin@gmail.com>
2016-07-18 11:01:30 +0000

    basic implementation

--- main.cc
+++ main.cc
@@ -1,16 +1,22 @@
 //
 // MLFRS (Machine Learning for Robotic simulations)
 //
+// Authors : Cormac Guerin, Tom Stewart.
+//
 
 #include "someclass.h"
 #include "physics/physics.h"
+#include "object_manager.h"
+#include "object_node.h"
 #include <stdio.h>
+#include <iostream>
 
 int main(int argc, char** argv)
 {
 	someclass *myclass = new someclass(argc);
-	physics *phy = new physics();
-
+	Physics *phy = new Physics();
+	ObjectManager *objectManager = new ObjectManager();
+	objectManager->addMdl("robot");
 	phy->init();
 
 	return 0 ;
